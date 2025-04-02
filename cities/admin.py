e93c44a35bde71d465e04e61e92efeaa37adf14b
dimalahno <dimalahno@rambler.ru>
2025-04-02 20:55:38 +0500

    add cities model

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from cities.models import City
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Город', unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
